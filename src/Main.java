5e131784b10f81f00ada6588db3e9120741a8b08
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-18 17:13:24 +0000

    Binary stub

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    /**
+     * Binaerzahlen
+     */
+    public static void main(String[] args){
+        // Transformieren Sie eine Ausgangszahl in den entsprechenden 
+        // Binaerwert und geben Sie diesen aus.
+        
+    }
+    
+}
