d0cd77a64824a5aff6c671c3e7d951d18aba4357
rybalt <arybaltschenko@web.de>
2019-07-19 14:42:31 +0000

    Erstellung

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,12 @@ public class Main {
     public static void main(String[] args){
         // Transformieren Sie eine Ausgangszahl in den entsprechenden 
         // Binaerwert und geben Sie diesen aus.
+        int Dezimalzahl = 30;
         
+        while(Dezimalzahl>0){
+            System.out.println(Dezimalzahl%2);
+            Dezimalzahl=Dezimalzahl/2;
+            }
     }
     
 }
