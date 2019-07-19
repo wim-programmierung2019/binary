1e6fdf04ba48abe8e1ff8bac1f0c7a69fe91d543
friej <julian-frie@gmx.de>
2019-07-19 15:15:19 +0000

    Binary mit Array

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,23 @@ public class Main {
     public static void main(String[] args){
         // Transformieren Sie eine Ausgangszahl in den entsprechenden 
         // Binaerwert und geben Sie diesen aus.
+        int Ausgangszahl = 241;
+        int ZahlZwei=Ausgangszahl;
+        int anzahlZahlen=0;
+        
+        while(Ausgangszahl != 0){
+            Ausgangszahl = Ausgangszahl / 2;
+            anzahlZahlen++;
+           
+        }
+        int zahlen[] = new int[anzahlZahlen];
+        for(int i=0; i < anzahlZahlen;i++){
+            zahlen[i] = ZahlZwei % 2;
+            ZahlZwei = ZahlZwei / 2;
+        }
+        for(int i = anzahlZahlen - 1; i>=0; i--){
+            System.out.print(zahlen[i]);
+        }
         
     }
     
