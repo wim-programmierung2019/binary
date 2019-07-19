2072b4bc5d39440e996dfd19c80517e3b694fc86
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-19 19:29:27 +0000

    Musterlösung (basierend auf Jonas Lösung)

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,14 @@ public class Main {
     public static void main(String[] args){
         // Transformieren Sie eine Ausgangszahl in den entsprechenden 
         // Binaerwert und geben Sie diesen aus.
+        int start = 10;
         
+        String binary = "";
+        while(start!=0 && start > 0){
+            binary = (start%2)+binary;
+            start = (start/2);
+            System.out.println(binary);
+        }
     }
     
 }
